605525fcf80ed77c5b4aa744df33684e72a8acf4
Áine Thompson <ainegthompson@gmail.com>
2021-06-02 22:10:30 +0000

    Completed part 4: Inheritance

--- oo.py
+++ oo.py
@@ -1,16 +1,20 @@
 # Create your classes here
 
-class Student(object):
+class Student:
 
     def __init__(self, first_name, last_name, address):
+        """ Initialize a student """
+
         self.first_name = first_name
         self.last_name = last_name
         self.address = address
 
 
-class Question():
+class Question:
 
     def __init__(self, question, correct_answer):
+        """ Initialize a question """
+
         self.question = question
         self.correct_answer = correct_answer
 
@@ -51,3 +55,94 @@ class Exam:
         return 100 * (score / len(self.questions))
 
 
+class StudentExam:
+
+    def __init__(self, student, test):
+        """ Initialize a student's exam """
+        self.student = student
+        self.test = test
+        self.score = 0
+
+    def take_test(self):
+        """ Administer exam and print score """
+
+        self.score = self.test.administer()
+
+        print(f'{self.student.first_name} scored {self.score} on their {self.test.name} exam.')
+
+def exam_example():
+    """ example student exam """
+
+    exam = Exam('midterm')
+
+    set_q = Question(
+        'What is the method for adding an element to a set?',
+        '.add()')
+    exam.add_question(set_q)
+
+    pwd_q = Question(
+        'What does pwd stand for?',
+        'print working directory')
+    exam.add_question(pwd_q)
+
+    list_q = Question(
+        'Python lists are mutable, iterable, and what?',
+        'ordered')
+    exam.add_question(list_q)
+
+    aine = Student('Aine', 'Thompson', 1234)
+
+    aine_exam = StudentExam(aine, exam)
+
+    aine_exam.take_test()
+
+
+class Quiz(Exam):
+    
+    def administer(self):
+        """ Administer Quiz and return score """
+
+        if super().administer() > 50:
+            return 1
+        else:
+            return 0
+
+
+class StudentQuiz(StudentExam):
+
+    def take_test(self):
+        self.score = self.test.administer()
+
+        if self.score == 1:
+            result = 'passed'
+        elif self.score == 0:
+            result = 'failed'
+
+        print(f'{self.student.first_name} {result} their {self.test.name} quiz')
+
+
+def quiz_example():
+    """ Example student quiz """
+
+    quiz = Quiz('Quiz 1')
+
+    set_q = Question(
+        'What is the method for adding an element to a set?',
+        '.add()')
+    quiz.add_question(set_q)
+
+    pwd_q = Question(
+        'What does pwd stand for?',
+        'print working directory')
+    quiz.add_question(pwd_q)
+
+    list_q = Question(
+        'Python lists are mutable, iterable, and what?',
+        'ordered')
+    quiz.add_question(list_q)
+
+    aine = Student('Aine', 'Thompson', 1234)
+
+    aine_quiz = StudentQuiz(aine, quiz)
+
+    aine_quiz.take_test()
